475ec4c35396535f4764975ff994c125c6ab1b58
osamafish <kingfish0214@gmail.com>
2020-12-03 19:03:11 -0700

    add email 3

--- config/environment.rb
+++ config/environment.rb
@@ -7,21 +7,24 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
 
 :address => 'smtp.gmail.com',
-
 :port => '587',
+
+# :address => 'localhost',
+# :port => '25',
 
 
 :authentication => :plain, #base64 encode 12:43:12 p.m. - Client: AUTH PLAIN ADEyMwAxMjM=
 
-# :user_name => ENV['SENDGRID_USERNAME'],
 
-# :password => ENV['SENDGRID_PASSWORD'],
+
+:user_name => ENV['SENDGRID_USERNAME'],
+:password => ENV['SENDGRID_PASSWORD'],
+
 
 # :authentication => :plain, #base64 encode 12:43:12 p.m. - Client: AUTH PLAIN ADEyMwAxMjM=
-:user_name => "kinglegofish2710@gmail.com",
-:password => "lyescenbhfednkrl",
 
-:domain => 'shashin-app.herokuapp.com/',
+:domain => 'shashin-app.herokuapp.com',
+# :domain => 'localhost',
 
 
 
